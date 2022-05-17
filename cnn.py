cf6cf19449a7837e555135e6f6ebac0a59829497
Nick Shaffer <nickyshaffer9@gmail.com>
2022-05-16 23:25:51 -0700

    Add clump xmps and change thresh value

--- cnn.py
+++ cnn.py
@@ -88,7 +88,7 @@ def main():
                     labels, boxes, scores = predictions
                     # show_labeled_image(image, boxes, labels)
 
-                    thresh=0.3
+                    thresh=0.4
                     filtered_indices=np.where(scores>thresh)
                     filtered_scores=scores[filtered_indices]
                     filtered_boxes=boxes[filtered_indices]
